c828f78f39915bfc1c5609542c379cdb175f3905
xzijian <xiezijian99@gmail.com>
2021-11-14 18:33:23 +0800

    fixed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from typing import BinaryIO
-from flask import Flask, render_template, url_for, request, redirect, jsonify
+from flask import Flask, render_template, url_for, request, redirect, jsonify, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import psycopg2
@@ -19,6 +19,9 @@ conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_
 def index():
     return render_template('index.html')
 
+## /customer end point is used to view customers as json 
+## and also used to query for youngest customers
+
 @app.route('/customer', methods = ['POST','GET'])
 def view_customer():
     if request.method == 'GET':
@@ -29,8 +32,6 @@ def view_customer():
                 s = "SELECT * FROM customer ORDER BY id"
                 cur.execute(s)
                 list_cust = cur.fetchall()
-                #return render_template('customer.html', list_cust = list_cust)
-                #return jsonify(json_list = list_cust)
                 content = {}
                 customer = []
                 for result in list_cust:
@@ -50,27 +51,13 @@ def view_customer():
                 return render_template('customer.html', list_cust = list_cust)
             except:
                 conn.rollback()
-                cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-                s = "SELECT * FROM customer ORDER BY id"
-                cur.execute(s)
-                list_cust = cur.fetchall()
-                return render_template('customer.html', list_cust = list_cust)
-    #elif request.headers['Content-Type'] == 'application/json':
-    #    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    #    s = "SELECT * FROM customer ORDER BY id"
-    #    cur.execute(s)
-    #    list_cust = cur.fetchall()
-    #    #return render_template('customer.html', list_cust = list_cust)
-    #    #return jsonify(json_list = list_cust)
-    #    content = {}
-    #    customer = []
-    #    for result in list_cust:
-    #        content = {'id' : result['id'], 'name' : result['name'], 'dob' : result['dob']}
-    #        customer.append(content)
-    #        content = {}
-    #    return jsonify(customer)
+                return "Invalid input"
     else:
         return render_template('customer.html')
+
+
+## /order end point is used to view orders as json and
+## also used to view orders by specific customers
 
 @app.route('/order', methods = ['POST','GET'])
 def view_order():
@@ -82,7 +69,6 @@ def view_order():
                 s = "SELECT * FROM orders ORDER BY datetime ASC"
                 cur.execute(s)
                 list_orders = cur.fetchall()
-                #return render_template('orders.html', list_orders = list_orders)
                 content = {}
                 orders = []
                 for result in list_orders:
@@ -101,25 +87,13 @@ def view_order():
                 return render_template('orders.html', list_orders = list_orders)
             except:
                 conn.rollback()
-                cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-                s = "SELECT * FROM orders ORDER BY datetime DESC"
-                cur.execute(s)
-                list_orders = cur.fetchall()
-                return render_template('orders.html', list_orders = list_orders)
+                return "Invalid input"
     else:
-    #    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    #    s = "SELECT * FROM orders ORDER BY datetime ASC"
-    #    cur.execute(s)
-    #    list_orders = cur.fetchall()
-    #    #return render_template('orders.html', list_orders = list_orders)
-    #    content = {}
-    #    orders = []
-    #    for result in list_orders:
-    #        content = {'item_name' : result['item_name'], 'item_price' : result['item_price'], 'datetime' : result['datetime'], 'customer_id' : result['customer_id']}
-    #        orders.append(content)
-    #        content = {}
-    #    return jsonify(orders)
         return render_template('orders.html')
+
+
+## /items end point is used to view items available for purchase
+## as json
 
 @app.route('/items')
 def view_items():
@@ -135,6 +109,9 @@ def view_items():
         content = {}
     return jsonify(orders)
 
+
+## /customer/create end point is used to create a new customer
+
 @app.route('/customer/create', methods=['POST','GET'])
 def create_customer():
     if request.method == 'POST':
@@ -144,12 +121,17 @@ def create_customer():
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         try:
             cur.execute("""INSERT INTO customer VALUES (%s, %s, %s)""", (cid,cname,dob))
-            return render_template('index.html')
+            flash('Customer created successfully')
+            conn.commit()
+            return redirect(url_for('index'))
         except:
             conn.rollback()
             return "Invalid input"
     else:
         return render_template('createCus.html')
+
+
+## /order/create end point is use to create a new order
 
 @app.route('/order/create', methods=['POST','GET'])
 def create_order():
@@ -159,21 +141,24 @@ def create_order():
         cid = request.form['cID2']
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         try:
-            cur.execute("""INSERT INTO orders
+            cur.execute("""INSERT INTO orders(order_id, item_id, item_name, item_price, customer_id)
                             VALUES (%s,%s,(SELECT item_name
 				                            FROM items
 				                            WHERE item_id = %s),
                                             (SELECT item_price
 									        FROM items
-									        WHERE item_id = %s),CURRENT_DATE,
+									        WHERE item_id = %s),
                                             (SELECT id
 		                                    FROM customer
 		                                    WHERE id = %s))""", (Oid, Iid, Iid, Iid, cid))
-            return render_template('index.html')
+            flash('Order created successfully')
+            conn.commit()
+            return redirect(url_for('index'))
         except:
             conn.rollback()
             return "Invalid input"
     else:
         return render_template('createOrd.html')
+        
 if __name__ == "__main__":
     app.run(debug=True)
